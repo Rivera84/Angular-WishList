250647e31eca67363ec5fabfbdfc2198f72badc1
Luis Rivera <luisrivera084@gmail.com>
2021-01-12 23:21:57 -0600

    creacion de boton preferido

--- src/app/components/destino-viaje/destino-viaje.component.ts
+++ src/app/components/destino-viaje/destino-viaje.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostBinding, EventEmitter, Output } from '@angular/core';
+import { DestinoViaje } from '../../models/destino-viajes.model';
 
 @Component({
   selector: 'app-destino-viaje',
@@ -6,11 +7,20 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./destino-viaje.component.css']
 })
 export class DestinoViajeComponent implements OnInit {
-  @Input() nombre:string;
+  @Input() destino: DestinoViaje;
+  @Input() position: number; //@Input('idx) --> sirve para renombrar las variables si es necesario
+  @HostBinding('attr.class') cssClass = 'col-md-4'//tener una vinculacion directa de un string de nuestro componente a un atributo del tag con el que se envuelve el contenido
+  @Output() clicked: EventEmitter<DestinoViaje>;
   constructor() {
+    this.clicked = new EventEmitter();
    }
 
   ngOnInit(): void {
+  }
+
+  ir():boolean{
+    this.clicked.emit(this.destino)
+    return false
   }
 
 }
--- src/app/components/lista-destinos/lista-destinos.component.ts
+++ src/app/components/lista-destinos/lista-destinos.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DestinoViaje } from '../../models/destino-viajes.model';
 @Component({
   selector: 'app-lista-destinos',
   templateUrl: './lista-destinos.component.html',
   styleUrls: ['./lista-destinos.component.css']
 })
 export class ListaDestinosComponent implements OnInit {
-  destinos: string[];
+  destinos: DestinoViaje[];
   constructor() { 
-    this.destinos = ['Barcelona','Buenos Aires','Lima'];
+    this.destinos = [];
   }
 
   ngOnInit(): void {
+  }
+
+  guardar(nombre:string, url:string): boolean{
+    this.destinos.push(new DestinoViaje(nombre, url));
+    console.log(this.destinos);
+    return false;
+  }
+  elegido(d:DestinoViaje){
+    this.destinos.forEach(function(x){x.setSelected(false); });
+    d.setSelected(true);
   }
 
 }
